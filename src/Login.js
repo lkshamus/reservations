9e704b9fb41ca1e807e2d991173f6072d852e3cc
Laura Shamus <lkshamus@gmail.com>
2019-01-23 13:29:12 -0700

    Add router

---
+++ src/Login.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  render() {
+    return (
+      <form>
+        <input
+          onChange={this.handleChange}
+          placeholder="Email"
+          value={this.state.email}
+        />
+        <input
+          onChange={this.handleChange}
+          placeholder="Password"
+          type="password"
+          value={this.state.password}
+        />
+        <button>submit</button>
+      </form>
+    );
+  }
+}
