85ac7ccc9d7da5846d1cbacfb31eca157e55b804
Laura Shamus <lkshamus@gmail.com>
2019-01-25 11:07:14 -0700

    Add fetch call on mainpage

--- src/MainPage.js
+++ src/MainPage.js
@@ -3,8 +3,26 @@ import React, { Component } from "react";
 export default class MainPage extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      dog: []
+    };
   }
+
+  componentDidMount = async () => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    const requestURL = `https://kennel-staging.herokuapp.com/api/v1/reservations/current`;
+    const optionsObj = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
+    const response = await fetch(requestURL, optionsObj);
+    const dogData = await response.json();
+    await this.setState({ dog: dogData });
+    console.log(dogData);
+    return dogData;
+  };
   render() {
     return (
       <div>
