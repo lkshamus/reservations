a4f2f9b39e4195a760958a7a6ad23c78a66c72ef
Laura Shamus <lkshamus@gmail.com>
2019-02-04 11:29:52 -0700

    Add post requests to utilities to clean up Booking

--- src/utilities.js
+++ src/utilities.js
@@ -18,3 +18,80 @@ export const fetchCall = async url => {
   }
   return await response.json();
 };
+
+export const petPost = async (petName, species, breed, color, dob, spayed, medications, feeding, shots) => {
+    const token = JSON.parse(localStorage.getItem("token"))
+    const requestURL = `http://kennel-staging.herokuapp.com/api/v1/pets`
+    const petBody = JSON.stringify({
+      "name": petName,
+      "species": species,
+      "breed": breed,
+      "color": color,
+      "dob": dob,
+      "spayed_neutered": spayed,
+      "medications": medications,
+      "feeding_instructions": feeding,
+      "shots": shots
+    })
+    const optionsObj = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: petBody,
+  }
+    console.log(petBody)
+    const response = await fetch(requestURL, optionsObj);
+    return await response.json();
+}
+
+export const ownerPost = async (ownerFn, ownerLn, address, cellNumber, homeNumber, ownerEmail) => {
+    const token = JSON.parse(localStorage.getItem("token"))
+    const requestURL = `http://kennel-staging.herokuapp.com/api/v1/pets`
+    const ownerBody = JSON.stringify({
+      "first_name": ownerFn,
+      "last_name": ownerLn,
+      "address": address,
+      "home_phone": homeNumber,
+      "cell_phone": cellNumber,
+      "email": ownerEmail 
+    })
+    const optionsObj = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: ownerBody,
+  }
+  console.log(ownerBody)
+    const response = await fetch(requestURL, optionsObj);
+    return await response.json();
+}
+
+export const vetPost = async (practiceName, vetName, vetAddress, vetPhone, vetEmail) => {
+    const token = JSON.parse(localStorage.getItem("token"))
+    const requestURL = `http://kennel-staging.herokuapp.com/api/v1/pets`
+    const vetBody = JSON.stringify({
+      "practice_name": practiceName,
+      "vet_name": vetName,
+      "address": vetAddress,
+      "phone": vetPhone,
+      "email": vetEmail 
+    })
+    const optionsObj = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: vetBody,
+  }
+  console.log(vetBody)
+    const response = await fetch(requestURL, optionsObj);
+    return await response.json();
+}
